51619e2931d21ab390e01a6b00d838de407aa4e0
Gift Mugweni <giftmugweni@gmail.com>
2026-08-23 23:38:40 +0200

    fix: avoid parens around fail count in PS5.1 string (parser sees ( as sub-expr start)

--- package/e2e/install-drill.ps1
+++ package/e2e/install-drill.ps1
@@ -112,7 +112,7 @@ Check 'VHD removed' (-not (Test-Path "$AppDir\data\distro\ext4.vhdx"))
 
 Write-Host ''
 if ($script:fail -gt 0) {
-  Write-Host "INSTALL DRILL FAILED ($script:fail checks)"
+  Write-Host "INSTALL DRILL FAILED - $script:fail checks"
   exit 1
 }
 Write-Host 'INSTALL DRILL PASSED'
